f38277c9cd08a1484b31d04a42922ea24737db99
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2021-05-02 22:37:55 +0200

    Hero: added last name & modified fontsfor first and last name. About me: added typing animation to present abilities

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation script
+var typed = new Typed(".metiers", {
+    strings: ["Developer", "System Admin", "Blogger", "Freelancer"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
